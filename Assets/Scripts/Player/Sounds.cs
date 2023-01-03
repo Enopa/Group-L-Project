abdde0140a77b5792bc516090005e44f6d4b3fbc
afjal-h <78381542+afjal-h@users.noreply.github.com>
2023-01-03 16:40:13 +0000

    movement sounds added
    added sprint and walk sounds

---
+++ Assets/Scripts/Player/Sounds.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    public AudioSource footstepsSound;
+    public AudioSource sprintSound;
+    public Movement movement;
+
+    void Update(){
+        if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.D)){
+            if(Input.GetKey(KeyCode.LeftShift)&& (movement.stamina>0)){
+                footstepsSound.enabled=false;
+                sprintSound.enabled=true;
+            }
+            else{
+
+                sprintSound.enabled=false;
+                footstepsSound.enabled=true;
+
+            }
+        }
+        else{
+            footstepsSound.enabled=false;
+        }
+
+    }
+
+}
